8e85fdb27bd7bb25a8260feda4923b937de3a962
Iñaki García Blasco <inaki@txorua.com>
2018-02-19 15:24:17 +0100

    Añado script

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  // Is this browser sufficiently modern to continue?
+  if ( !( "querySelector" in document && "addEventListener" in window && "getComputedStyle" in window) ) {
+    () => {return}
+  }
+
+  var fali = document.createElement('span')
+  fali.classList.add('fa-li')
+
+  var fa = document.createElement('i')
+  i.classList.add('fas')
+  i.classList.add('fa-check')
+  fali.appenfChild(fa)
+
+  var uls = document.querySelectorAll('ul')
+  foreach(ul of uls) {
+    ul.classList.add('fa-ul')
+  }
+
+  var lis = document.querySelectorAll('li')
+  foreach (li of lis) {
+    li.appenfChild(fali)
+  }
+
+
+})
