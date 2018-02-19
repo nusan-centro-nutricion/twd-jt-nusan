00167efaa18ab8954e316bd98ed9fd8f9ad3910e
Iñaki García Blasco <inaki@txorua.com>
2018-02-19 18:35:16 +0100

    Cambio código

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,12 @@ document.addEventListener('DOMContentLoaded', function (event) {
     () => { return }
   }
 
-  var uls = document.querySelectorAll('main ul')
+  var uls = document.querySelectorAll('main section:not(.hero) ul')
   for (let ul of uls) {
     ul.classList.add('fa-ul')
   }
 
-  var lis = document.querySelectorAll('main li')
+  var lis = document.querySelectorAll('main section:not(.hero) li')
   for (let li of lis) {
     let fali = document.createElement('span')
     fali.classList.add('fa-li')
